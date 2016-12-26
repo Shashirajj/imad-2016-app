e7f3794684e9fcb2e703948f51a077b8686c4394
Shashirajj <rajveershashi12@gmail.com>
2016-12-26 23:42:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/ui/style.css', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
